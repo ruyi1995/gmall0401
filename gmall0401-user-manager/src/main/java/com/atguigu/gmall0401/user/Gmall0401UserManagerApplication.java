06b25073e6131d5181b81ba85e0b1898ee290c36
谌如意 <q1060764752@qq.com>
2020-09-28 23:03:30 +0800

    修改 readme

---
+++ gmall0401-user-manager/src/main/java/com/atguigu/gmall0401/user/Gmall0401UserManagerApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall0401.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0401UserManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gmall0401UserManagerApplication.class, args);
+	}
+
+}
